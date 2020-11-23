c262383c4758658cfbcf369dbcf5d9358f64aa54
Jimmy Lam <JimmyLam978@gmail.com>
2020-11-23 11:32:06 -0500

    midi works

--- pages/index.js
+++ pages/index.js
@@ -3,61 +3,56 @@ import React, { useRef, useEffect } from "react";
 import dynamic from "next/dynamic";
 import { Container, Box, Grid, Button } from "@material-ui/core";
 
-const MIDISounds = dynamic(() => import("midi-sounds-react"), { ssr: false });
+//const MIDISounds = dynamic(() => import("midi-sounds-react"), { ssr: false });
+import { instrument } from "soundfont-player";
 
 export default function Home() {
-	let midiSounds;
-	function playNote() {
-		midiSounds.retry();
-		//midiSounds.playChordNow(3, [60], 2.5);
-	}
+	let pianoRef = useRef(null);
 
 	useEffect(() => {
-		console.log("hi", midiSounds);
-	}, [midiSounds]);
+		instrument(new AudioContext(), "acoustic_grand_piano", {
+			soundfont: "MusyngKite",
+		}).then((piano) => {
+			pianoRef.current = piano;
+		});
+	}, []);
+
+	function playNote() {
+		pianoRef.current.play("C4");
+	}
 
 	return (
-		<div className="root" id="root">
-			<Container>
-				<Box
-					display="flex"
-					height="100vh"
-					justifyContent="center"
+		<Container>
+			<Box
+				display="flex"
+				height="100vh"
+				justifyContent="center"
+				alignItems="center"
+			>
+				<Grid
+					container
+					direction="row"
+					spacing={1}
+					justify="center"
 					alignItems="center"
 				>
-					<Grid
-						container
-						direction="row"
-						spacing={1}
-						justify="center"
-						alignItems="center"
-					>
-						<Grid item>
-							<Button onClick={playNote} variant="outlined" color="primary">
-								A
-							</Button>
-						</Grid>
-						<Grid item>
-							<Button variant="outlined" color="primary">
-								B
-							</Button>
-						</Grid>
-						<Grid item>
-							<Button variant="outlined" color="primary">
-								C
-							</Button>
-						</Grid>
+					<Grid item>
+						<Button onClick={playNote} variant="outlined" color="primary">
+							A
+						</Button>
 					</Grid>
-				</Box>
-			</Container>
-			<MIDISounds
-				ref={(ref) => {
-					console.log("hello", ref);
-					midiSounds = ref;
-				}}
-				appElementName="root"
-				instruments={[3]}
-			/>
-		</div>
+					<Grid item>
+						<Button variant="outlined" color="primary">
+							B
+						</Button>
+					</Grid>
+					<Grid item>
+						<Button variant="outlined" color="primary">
+							C
+						</Button>
+					</Grid>
+				</Grid>
+			</Box>
+		</Container>
 	);
 }
