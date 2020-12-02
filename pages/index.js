2f642e04dac8b47cd5f3e59de49e77e6c3f9487b
Jimmy Lam <JimmyLam978@gmail.com>
2020-12-02 16:32:03 -0500

    basic layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import React, { useEffect, useRef } from "react";
-import { Container, Box, Grid, Button } from "@material-ui/core";
 import useIntervalTrainer from "./hooks/useIntervalTrainer";
+
+import { Container, Box, Grid, Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 
 import { instrument } from "soundfont-player";
 
+const useStyles = makeStyles((theme) => ({
+	gameChoices: {
+		"& > *": {
+			margin: theme.spacing(2, 1),
+		},
+	},
+	setting: {
+		marginTop: theme.spacing(3),
+		marginBottom: theme.spacing(2),
+	},
+}));
+
 const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
 export default function Home() {
+	const classes = useStyles();
 	const pianoRef = useRef(null);
 	let ac;
 
@@ -20,18 +38,53 @@ export default function Home() {
 		});
 	}, []);
 
-	async function playNote(note) {
-		pianoRef.current.play(36);
-	}
-
 	return (
 		<Box
 			flexGrow={1}
 			display="flex"
 			justifyContent="center"
 			alignItems="center"
+			textAlign="center"
 		>
 			<Container>
+				<Typography variant="h3">Interval Training</Typography>
+				<Box
+					display="flex"
+					justifyContent="center"
+					className={classes.gameChoices}
+				>
+					<Button
+						variant="contained"
+						color="secondary"
+						startIcon={<PlayArrowIcon />}
+					>
+						Hear Again
+					</Button>
+					<Button
+						variant="outlined"
+						color="secondary"
+						endIcon={<ArrowForwardIcon />}
+					>
+						Skip
+					</Button>
+				</Box>
+				<Typography variant="h5">Pick your answer</Typography>
+				<Box
+					display="flex"
+					justifyContent="center"
+					className={classes.gameChoices}
+				>
+					{semitonesChoices.map((choice) => {
+						return (
+							<Button variant="outlined" color="default">
+								{semitones[choice - 1]}
+							</Button>
+						);
+					})}
+				</Box>
+				<Typography variant="h4" className={classes.setting}>
+					Settings
+				</Typography>
 				<Grid
 					container
 					direction="row"
